698b8c976eeb67b1446a4ebcba6853820ee15fdc
Marcos Rolando <marcosrolando.mr@gmail.com>
2022-10-07 19:54:37 -0300

    Add support for paths

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
