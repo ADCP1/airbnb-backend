c67993299de5fc2a222badced0f91ef69470410b
Marcos Rolando <marcosrolando.mr@gmail.com>
2022-10-07 21:29:55 -0300

    Add health endpoint

--- src/controllers/router.ts
+++ src/controllers/router.ts
@@ -2,5 +2,6 @@ import { Express } from 'express';
 import { AuthRouter } from './auth';
 
 export function registerRouters(app: Express) {
+  app.get('/health', (_, res) => res.send());
   app.use('/auth', AuthRouter());
 }
