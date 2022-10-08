9228c7063c0a7c910fd7ec08ffe5ea05a461e3ac
Marcos Rolando <marcosrolando.mr@gmail.com>
2022-10-08 17:04:19 -0300

    Add http logger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import pino from 'express-pino-logger';
 import { snakeToCamelcaseKeys } from './shared';
 import { config } from './config';
 import { registerRouters } from './controllers';
@@ -12,6 +13,7 @@ async function main() {
 
   const app = express();
 
+  app.use(pino());
   app.use(cors());
   app.use(express.json());
   app.use(snakeToCamelcaseKeys());
