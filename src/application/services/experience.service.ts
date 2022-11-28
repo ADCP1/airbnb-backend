1f2c3b4ab3d4bf099790c77950374e77226fad87
Marcos Rolando <marcosrolando.mr@gmail.com>
2022-11-28 13:18:51 -0300

    Change default capacity to 0

--- src/application/services/experience.service.ts
+++ src/application/services/experience.service.ts
@@ -58,7 +58,7 @@ class ExperienceService implements IExperienceService {
     const experience = new Experience({
       ...experienceDto,
       ownerId: organizer.id!,
-      capacity: experienceDto.capacity ?? -1,
+      capacity: experienceDto.capacity ?? 0,
     });
     await this.experienceRepository.save(experience);
     return ExperienceFactory.toDto(experience);
@@ -75,7 +75,7 @@ class ExperienceService implements IExperienceService {
       throw new DomainException('Experience does not belong to the user');
     }
     if (experience.type == ExperienceType.Online) {
-      experienceDto.capacity = -1;
+      experienceDto.capacity = 0;
     }
     const updatedExperience = new Experience({
       ...experience,
